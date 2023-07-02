2dbddf42345b3a36313e0ec3776b4a45e59b150b
Dmytro Petyshyn <dev6012@gmail.com>
2023-07-02 12:12:00 +0300

    add LS and CustomHook to handle it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 
+import { useLocalStorage } from 'hooks/useLocalStorage';
 import {
   countPositiveFeedbackPercentage,
   countTotalFeedback,
 } from './utils/stats';
 
 export const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [good, setGood] = useLocalStorage('good', '');
+  const [neutral, setNeutral] = useLocalStorage('neutral', '');
+  const [bad, setBad] = useLocalStorage('bad', '');
 
   const stats = { good, neutral, bad };
 
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+export const useLocalStorage = (key, defaultValue) => {
+  const [state, setState] = useState(() => {
+    return Number(JSON.parse(window.localStorage.getItem(key))) ?? defaultValue;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, setState];
+};
